4871fb36e111e918bf169c33e896178c171ca591
alihamza <alihamzavt445@gmail.com>
2025-06-17 17:23:48 +0500

    google adsense meta link attach

--- app/_document.js
+++ app/_document.js
@@ -9,7 +9,8 @@ const _document = () => {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
     <link href="https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,100..900;1,100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet"/>
     <meta name="x-theme-primary" content={/* get from headers */ ""} />
-    <meta name="x-theme-primary-foreground" content={/* get from headers */ ""} />
+      <meta name="x-theme-primary-foreground" content={/* get from headers */ ""} />
+      <meta name="google-adsense-account" content="ca-pub-7182622830003276"></meta>
 
     
 
